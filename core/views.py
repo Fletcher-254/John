c8c19bce67a02701b27b3f3e65ca2080f82a78be
Fletcher-254 <fletcher.nyawira@student.moringaschool.com>
2026-07-01 12:04:34 +0300

    Configure project correctly for Render deployment

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import JsonResponse
 
 def home(request):
-    return JsonResponse({"message": "Nyutu Systems API is running"})
+    return JsonResponse({"message": "Welcome to Nyutu"})
