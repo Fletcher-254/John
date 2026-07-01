91277aef5dc9a08f927187771748dd14124a0e2f
Fletcher-254 <fletcher.nyawira@student.moringaschool.com>
2026-07-01 10:52:18 +0300

    Configure project correctly for Render deployment

--- Chuka/celery.py
+++ Chuka/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chuka.settings')
 
-app = Celery('your_project')
+app = Celery('Chuka')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
